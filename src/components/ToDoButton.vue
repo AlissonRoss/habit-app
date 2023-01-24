bcc06daf88e23d0d3179e0d283e6930e72e98a89
Alisson Ross <alisson@alisson.me>
2023-01-24 10:40:10 -0800

    Button and Menu for ToDo Task List

---
+++ src/components/ToDoButton.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "ToDoButton",
+    methods: {
+        // send click information to parent
+        openToDoListMenu() {
+            this.$emit("openToDoListMenu")      
+        }
+    }
+}
+</script>
+<template>
+    <div id="toDoButton">
+        <button v-on:click=(openToDoListMenu)>To-Do List</button>   
+    </div>
+</template>
---
+++ src/components/ToDoListMenu.vue
@@ -0,0 +1,52 @@
+<script>
+    export default {
+        name: "ToDoListMenu",
+        methods: {
+        // send click information to parent
+            closeToDoListMenu () {
+                if (!this.mouseInMenu) {
+                    this.$emit("closeToDoListMenu")
+                }
+            }
+        }
+    }
+</script>
+<template>
+    <div id="add-task-menu">
+        <div id="background" v-on:click=(closeToDoListMenu)>
+            <div id="menu" @mouseover="mouseInMenu=true" @mouseleave="mouseInMenu=false">
+                <form>
+                    <input type="checkbox" id="task-1">
+                        <label for="task-1"> Task #1 </label><br>
+                    <input type="checkbox" id="task-2">
+                        <label for="task-2"> Task #2 </label><br>        
+                    <input type="checkbox" id="task-3">
+                    <label for="task-3"> Task #3 </label><br>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+/* background becomes unclickable and a translucent grey cover */
+#background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+/* menu is a white box with a border */
+#menu {
+    width: 500px;
+    height: 300px;
+    background-color: white;
+    border: 1px solid black;
+    z-index: 2;
+}
+</style>
